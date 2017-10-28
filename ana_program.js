b4569fc6c6fda883e472a27ae5e48066da17e0cb
m.y software <aboo67671@gmail.com>
2017-10-29 02:24:01 +0300

    Update ana_program.js

--- ana_program.js
+++ ana_program.js
@@ -3,6 +3,7 @@ var secilen_index = 0;
 var arac_kutusu_c;
 var ozellik_menu;
 var deneme_p;
+var deneme_sim;
 
 function deneme_line (x1,x2,y1,y2){
 	background(255);
@@ -14,31 +15,38 @@ function deneme_line (x1,x2,y1,y2){
 function setup(){
 	createCanvas(1366,768);
 	background(255);
-	_deneme[0]=new algroitma_sema(100,100,50,100,1,"mustafa yılmaz deneme");
-	_deneme[1]=new algroitma_sema(200,200,50,100,1,"şunu bi em");
+	_deneme[0]=new algroitma_sema(100,100,40,100,1,"mustafa yılmaz deneme");
+	_deneme[1]=new algroitma_sema(200,200,40,100,1,"şunu bi em");
 	arac_kutusu_c = new arac_kutusu(10,10);
 	ozellik_menu = new ozellik_menu(1000,100);
-	_deneme[2]=new algroitma_sema(300,300,50,100,3,"BAŞLA");
-	_deneme[3]=new algroitma_sema(400,400,50,100,3,"DURDUR");
-	_deneme[4]=new algroitma_sema(500,500,50,100,2,"asdas4 asdas dasda sd");
+	_deneme[2]=new algroitma_sema(300,300,40,100,3,"BAŞLAT");
+	_deneme[3]=new algroitma_sema(400,400,40,100,3,"DURDUR");
+	_deneme[4]=new algroitma_sema(500,500,40,100,2,"asdas4 asdas dasda sd");
+	_deneme[5]=new algroitma_sema(800,500,40,100,1,"dddd");
+	deneme_sim = new simülasyon();
+	//deneme_sim.analiz();
+	ozellik_menu.ciz();
 }
 
 function draw(){
 //background(255);
 clear();
 arac_kutusu_c.ciz();	
-ozellik_menu.ciz();
-
+//deneme_sim.baslat_durdur_ara();
 for(let a = 0 ; a<_deneme.length; a++){
 	_deneme[a].ciz();
 }
 
+deneme_sim.ciz();
+
 for(let a=0; a<_bag.length;a++){
 	_bag[a].ciz();
 }
+//ozellik_menu.guncelle();
 }
 
 function mousePressed(){	
+//deneme = ozellik_menu.degisken_adi_textbox.value() + '=' +ozellik_menu.değişken_deger.value();
 for(var a = 0 ; a<_deneme.length; a++){
 	_deneme[a].sec();
 	if(_deneme[a].secildi==1){
